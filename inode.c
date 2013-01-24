5316e02386794382002410000ad0ee5564f983c1
raczzoli <racz.zoli@gmail.com>
2013-01-25 01:49:23 +0200

    added incomplete read_inode function

--- inode.c
+++ inode.c
@@ -1,24 +1,75 @@
+#include <linux/buffer_head.h>
 #include "inode.h"
 
 
-/* file operations */
-static struct file_operations dir_fops;
-
-/* inode operations */
-static struct inode_operations dir_iops;
+/*
+ * reads the inode from the disk by inode number
+ */
+//static struct testfs_inode *read_inode(struct super_block *sb, unsigned long ino, struct buffer_head **bh_);
 
 
 struct inode *inode_iget(struct super_block *sb, unsigned long ino)
 {
-  struct inode *inode;
-	inode = iget_locked(sb, ino);
+	struct inode *inode;
+//	struct testfs_inode *raw_inode;
+//	struct buffer_head *bh;
+//	long ret = -EIO;
+
+	inode 		= iget_locked(sb, ino);
+
+	/*
+	raw_inode 	= read_inode(sb, ino, &bh); 
+
+	if (raw_inode == NULL)
+	{
+		ret = -EIO; 
+		goto err_read_inode;
+	}
+
+	inode->i_mode = raw_inode->i_mode;
+	*/
+
+	// this needs to be replaced with raw_inode->i_mode, but until read_inode
+	// works this hardcode lets us at least mount the file system
+	inode->i_mode = S_IFDIR;
 
 	if (S_ISDIR(inode->i_mode))
 	{
-		inode->i_op 	= &dir_iops;
-		inode->i_fop 	= &dir_fops;
+		inode->i_op     = &testfs_dir_iops; // set the inode ops	
+		inode->i_fop    = &testfs_dir_fops;
+	}
+	else if (S_ISREG(inode->i_mode))
+	{
+		inode->i_op     = &testfs_file_iops; // set the inode ops
+                inode->i_fop    = &testfs_file_fops;
 	}
 
 	return inode;
+
+//err_read_inode:
+//	brelse(bh);
+
+//	return ERR_PTR(ret);
 }
 
+
+/* block and b_offset needs to be calculated, but I`m too tired to do this now :(
+static struct testfs_inode *read_inode(struct super_block *sb, unsigned long ino, struct buffer_head **bh_)
+{
+	struct buffer_head *bh;
+	unsigned long block;
+	unsigned long b_offset;
+	block 	= 0; 
+	*bh_ 	= NULL;
+
+	if (!(bh = sb_bread(sb, block)))
+	{
+		printk(KERN_INFO "testfs: error reading inode block from disk... inode number: %lu.\n", ino);
+		return NULL;
+	}
+
+	*bh_ = bh;
+
+	return (struct testfs_inode *) (bh->b_data + b_offset);
+}
+*/
--- inode.h
+++ inode.h
@@ -3,6 +3,23 @@
 
 #include <linux/fs.h>
 
+
+/* dir.c */
+extern const struct file_operations testfs_dir_fops;
+extern const struct inode_operations testfs_dir_iops;
+
+
+/* file.c */
+extern const struct file_operations testfs_file_fops;
+extern const struct inode_operations testfs_file_iops;
+
+
+
+struct testfs_inode {
+	__le16  i_mode;
+};
+
+
 struct inode *inode_iget(struct super_block *sb, unsigned long ino);
 
 #endif /* INODE_H */
