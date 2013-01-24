d05ac0ea31aa5f004e0bd6727443214a97720fc4
raczzoli <racz.zoli@gmail.com>
2013-01-25 00:27:54 +0200

    added dir.c and file.c

---
+++ dir.c
@@ -0,0 +1,8 @@
+#include <linux/fs.h>
+
+/* dir opertions */
+const struct file_operations testfs_dir_fops;
+
+/* dir inode operations */
+const struct inode_operations testfs_dir_iops;
+
---
+++ file.c
@@ -0,0 +1,7 @@
+#include <linux/fs.h>
+
+/* file operations */
+const struct file_operations testfs_file_fops;
+
+/* file inode operations */
+const struct inode_operations testfs_file_iops;
