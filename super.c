1022eee7ebbdd78940a2afd153f51303797ebe62
Prashant P Shah <pshah.mumbai@gmail.com>
2013-01-25 22:07:11 +0530

    Read block size and check magic number from superblock

--- super.c
+++ super.c
@@ -1,5 +1,6 @@
 #include <linux/buffer_head.h>
 #include <linux/fs.h>
+#include <linux/slab.h>
 
 #include "testfs.h"
 #include "super.h"
@@ -14,7 +15,7 @@ static void put_super(struct super_block *);
 
 
 static struct super_operations testfs_super_ops = {
-  .put_super 	= put_super
+	.put_super 	= put_super
 };
 
 
@@ -29,6 +30,7 @@ static int fill_super(struct super_block *sb, void *data, int silent)
 {
 	struct buffer_head *bh 	= NULL;
 	struct inode *root  = NULL;
+	struct testfs_superblock *testfs_sb = NULL;
 	int ret = -1;
 	
         if (!(bh = sb_bread(sb, TESTFS_SUPER_BLOCK_NUM)))
@@ -36,10 +38,29 @@ static int fill_super(struct super_block *sb, void *data, int silent)
         	printk(KERN_INFO "testfs: unable to read superblock.\n");
                	goto err;
        	}
-
 	printk(KERN_INFO "testfs: superblock read successfuly.\n");
 
-	sb->s_blocksize 	= TESTFS_BLOCK_SIZE;
+	testfs_sb = kmalloc(sizeof(*testfs_sb), GFP_KERNEL);
+	if (!testfs_sb) {
+		printk(KERN_INFO "testfs: unable to read superblock.\n");
+		goto err;
+	}
+	memcpy(testfs_sb, bh->b_data, sizeof(*testfs_sb));
+
+	printk(KERN_INFO "testfs: magic=%d block_size=%d "
+		"block_count=%d itable=%d itable_size=%d block_bitmap=%d\n",
+		testfs_sb->magic, testfs_sb->block_size, testfs_sb->block_count,
+		testfs_sb->itable, testfs_sb->itable_size, testfs_sb->block_bitmap);
+
+	/* Check whether valid testfs */
+	if (testfs_sb->magic != TESTFS_MAGIC_NUM) {
+		printk(KERN_ERR "testfs: not a testfs filesystem\n");
+		goto err;
+	}
+
+	/* TODO: Use a in-memory structure */
+	sb->s_fs_info		= testfs_sb;
+	sb->s_blocksize 	= testfs_sb->block_size;
 	sb->s_blocksize_bits 	= 10;
 	sb->s_magic		= TESTFS_MAGIC_NUM;
 	sb->s_op		= &testfs_super_ops;
@@ -61,6 +82,8 @@ err:
 	 	brelse(bh);
 	if (root)
         	iput(root);
+	if (testfs_sb)
+		kfree(testfs_sb);
 
 	return ret;
 }
@@ -68,6 +91,8 @@ err:
 
 static void put_super (struct super_block *sb)
 {
-
+	if (sb->s_fs_info) {
+		kfree(sb->s_fs_info);
+	}
 }
 
