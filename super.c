a0c612c47b4e2e1174ea3c7a31a30ad1871284ae
Prashant P Shah <pshah.mumbai@gmail.com>
2013-01-25 23:13:45 +0530

    Add in-memory structure for file system

--- super.c
+++ super.c
@@ -31,21 +31,29 @@ static int fill_super(struct super_block *sb, void *data, int silent)
 	struct buffer_head *bh 	= NULL;
 	struct inode *root  = NULL;
 	struct testfs_superblock *testfs_sb = NULL;
+	struct testfs_info *testfs_i = NULL;
 	int ret = -1;
 	
         if (!(bh = sb_bread(sb, TESTFS_SUPER_BLOCK_NUM)))
 	{
-        	printk(KERN_INFO "testfs: unable to read superblock.\n");
+		printk(KERN_ERR "testfs: unable to read superblock.\n");
                	goto err;
        	}
 	printk(KERN_INFO "testfs: superblock read successfuly.\n");
 
 	testfs_sb = kmalloc(sizeof(*testfs_sb), GFP_KERNEL);
 	if (!testfs_sb) {
-		printk(KERN_INFO "testfs: unable to read superblock.\n");
+		printk(KERN_ERR "testfs: failed to allocate superblock memory\n");
 		goto err;
 	}
 	memcpy(testfs_sb, bh->b_data, sizeof(*testfs_sb));
+
+	testfs_i = kmalloc(sizeof(*testfs_i), GFP_KERNEL);
+	if (!testfs_i) {
+		printk(KERN_ERR "testfs: failed to allocate filesystem info memory\n");
+		goto err;
+	}
+	memset(testfs_i, 0x00, sizeof(*testfs_i));
 
 	printk(KERN_INFO "testfs: magic=%d block_size=%d "
 		"block_count=%d itable=%d itable_size=%d block_bitmap=%d\n",
@@ -58,32 +66,42 @@ static int fill_super(struct super_block *sb, void *data, int silent)
 		goto err;
 	}
 
-	/* TODO: Use a in-memory structure */
-	sb->s_fs_info		= testfs_sb;
+	testfs_i->sb		= testfs_sb;
+	testfs_i->bh		= bh;
+
+	sb->s_fs_info		= testfs_i;
 	sb->s_blocksize 	= testfs_sb->block_size;
 	sb->s_blocksize_bits 	= 10;
 	sb->s_magic		= TESTFS_MAGIC_NUM;
 	sb->s_op		= &testfs_super_ops;
 
 	root = inode_iget(sb, TESTFS_ROOT_INODE_NUM);
-
 	if (!root) 
 	{
-		printk(KERN_INFO "testfs: inode_iget failed in fill_super!\n");
+		printk(KERN_ERR "testfs: inode_iget failed in fill_super!\n");
 		goto err;	
 	}
+	testfs_i->root = root;
 
-	sb->s_root = d_make_root(root);	
+	sb->s_root = d_make_root(root);
+	if (!sb->s_root) {
+		printk(KERN_ERR "testfs: get root dentry failed\n");
+		goto err;
+	}
+
+	printk(KERN_INFO "testfs: mounted testfs file system\n");
 
 	return 0;
 
 err:
-	if (bh)
-	 	brelse(bh);
 	if (root)
         	iput(root);
+	if (testfs_i)
+		kfree(testfs_i);
 	if (testfs_sb)
 		kfree(testfs_sb);
+	if (bh)
+		brelse(bh);
 
 	return ret;
 }
@@ -91,7 +109,23 @@ err:
 
 static void put_super (struct super_block *sb)
 {
+	struct testfs_info *testfs_i = NULL;
+
 	if (sb->s_fs_info) {
+		testfs_i = sb->s_fs_info;
+
+		if (testfs_i->root) {
+			iput(testfs_i->root);
+		}
+
+		if (testfs_i->sb) {
+			kfree(testfs_i->sb);
+		}
+
+		if (testfs_i->bh) {
+			brelse(testfs_i->bh);
+		}
+
 		kfree(sb->s_fs_info);
 	}
 }
