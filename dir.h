5db3fb7d6e2a565eb2830b1c019277956cf01f35
raczzoli <racz.zoli@gmail.com>
2013-01-28 17:05:16 -0700

    added type to testfs_dir_entry

--- dir.h
+++ dir.h
@@ -9,6 +9,7 @@ struct testfs_dir_entry {
 	__le32 inode_number;	/* Inode number */
 	__le32 name_len;	/* File name length */
 	char name[20];		/* File name */
+	__u8 type;		/* DT_DIR or DT_REG */
 };
 
 extern const struct file_operations testfs_dir_fops;
