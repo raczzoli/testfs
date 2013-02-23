5ebb20a9349e9a0d297928972d0189ef7d2c3f86
raczzoli <racz.zoli@gmail.com>
2013-02-23 03:48:10 -0700

    changed how inode and block bitmaps are marked as dirty

--- bitmap.c
+++ bitmap.c
@@ -1,11 +1,11 @@
 #include "bitmap.h"
 #include "testfs.h"
 #include "super.h"
-
+#include <linux/buffer_head.h>
 
 
 // checks for the next free bit in bitmap (used for block and inode bitmaps)
-static inline int get_free_bit_from_bitmap(struct super_block *sb, char *bitmap, int start_pos, int *num);
+static inline int get_free_bit_from_bitmap(struct super_block *sb, struct buffer_head *bitmap_bh, int start_pos, int *num);
 
 
 inline int bitmap_get_free_data_block_num(struct super_block *sb, int *block_num)
@@ -14,8 +14,8 @@ inline int bitmap_get_free_data_block_num(struct super_block *sb, int *block_num
         struct testfs_superblock *testfs_sb     = testfs_info->sb;
         int start_block_num                     = testfs_sb->itable + testfs_sb->itable_size;
 
-	printk(KERN_INFO "testfs: requesting free data block\n");
-        return get_free_bit_from_bitmap(sb, testfs_info->block_bitmap, start_block_num, block_num);
+	printk(KERN_INFO "testfs: get free data block num\n");
+        return get_free_bit_from_bitmap(sb, testfs_info->block_bmp_bh, start_block_num, block_num);
 }
 
 
@@ -24,8 +24,8 @@ inline int bitmap_get_free_inode_num(struct super_block *sb, int *inode_num)
         struct testfs_info *testfs_info         = (struct testfs_info *)sb->s_fs_info;
         struct testfs_superblock *testfs_sb     = testfs_info->sb;
 
-	printk(KERN_INFO "testfs: requesting free inode number\n");
-        return get_free_bit_from_bitmap(sb, testfs_info->inode_bitmap, testfs_sb->itable, inode_num);
+	printk(KERN_INFO "testfs: get free inode num\n");
+        return get_free_bit_from_bitmap(sb, testfs_info->inode_bmp_bh, testfs_sb->itable, inode_num);
 }
 
 
@@ -40,8 +40,8 @@ inline int bitmap_free_data_block_num(struct super_block *sb, int block_num)
         int byte  = (block / 8);
         int bit   = block % 8;
 
-        testfs_info->block_bitmap[byte] = testfs_info->block_bitmap[byte] ^ (mask >> bit);
-	sb->s_dirt = 1;
+        testfs_info->block_bmp_bh->b_data[byte] = testfs_info->block_bmp_bh->b_data[byte] ^ (mask >> bit);
+	mark_buffer_dirty(testfs_info->block_bmp_bh);
 
         return 0;
 }
@@ -52,20 +52,20 @@ inline int bitmap_free_inode_num(struct super_block *sb, int inode_num)
         struct testfs_info *testfs_info         = (struct testfs_info *)sb->s_fs_info;
         struct testfs_superblock *testfs_sb     = testfs_info->sb;
         int start_inode_num                     = testfs_sb->itable;
-
+	
         int mask  = 0x80;
         int block = (inode_num - start_inode_num);
         int byte  = (block / 8);
         int bit   = block % 8;
 
-        testfs_info->inode_bitmap[byte] = testfs_info->inode_bitmap[byte] ^ (mask >> bit);
-	sb->s_dirt = 1;
+        testfs_info->inode_bmp_bh->b_data[byte] = testfs_info->inode_bmp_bh->b_data[byte] ^ (mask >> bit);
+	mark_buffer_dirty(testfs_info->inode_bmp_bh);
 
         return 0;
 }
 
 
-static inline int get_free_bit_from_bitmap(struct super_block *sb, char *bitmap, int start_pos, int *num)
+static inline int get_free_bit_from_bitmap(struct super_block *sb, struct buffer_head *bitmap_bh, int start_pos, int *num)
 {
         struct testfs_info *testfs_info         = (struct testfs_info *)sb->s_fs_info;
         struct testfs_superblock *testfs_sb     = testfs_info->sb;
@@ -76,12 +76,12 @@ static inline int get_free_bit_from_bitmap(struct super_block *sb, char *bitmap,
         int block_counter                       = 0;
 
         for (i=0;i<block_size;i++) {
-                byte = bitmap[i];
+                byte = bitmap_bh->b_data[i];
                 for (bit=0;bit<7;bit++) {
                         byte = byte << (bit == 0 ? 0 : 1);
                         if ((byte & mask) < 1) {
                                 /* we found the first free block, so we mark it as used */
-                                bitmap[i] = bitmap[i] ^ (mask >> bit);
+				bitmap_bh->b_data[i] = bitmap_bh->b_data[i] ^ (mask >> bit); 
                                 goto block_found;
                         }
                         block_counter++;
@@ -92,8 +92,7 @@ static inline int get_free_bit_from_bitmap(struct super_block *sb, char *bitmap,
 
 block_found:
         *num = block_counter + start_pos;
-	printk(KERN_INFO "testfs: found free block %d\n", *num);
-	sb->s_dirt = 1;
+	mark_buffer_dirty(bitmap_bh);
         return 0;
 
 }
