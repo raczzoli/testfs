ab9175822bcc1d35480959fd34c502aac943fc4a
raczzoli <racz.zoli@gmail.com>
2013-03-03 16:24:34 -0700

    added function pointers to testfs_file_ops

--- file.c
+++ file.c
@@ -1,7 +1,25 @@
 #include <linux/fs.h>
 
+
+
+int testfs_fsync(struct file *file, loff_t start, loff_t end, int datasync)
+{
+	printk(KERN_INFO "testfs: testfs_fsync called\n");
+	return 0;
+}
+
+
 /* file operations */
-const struct file_operations testfs_file_fops;
+const struct file_operations testfs_file_fops = {
+	.llseek 	= generic_file_llseek,
+	.aio_read 	= generic_file_aio_read,
+	.aio_write 	= generic_file_aio_write,
+//	.read		= generic_file_read,
+//	.write		= generic_file_write,
+	.mmap		= generic_file_mmap,
+	.open		= generic_file_open,
+	.fsync 		= testfs_fsync
+};
 
 /* file inode operations */
 const struct inode_operations testfs_file_iops;
