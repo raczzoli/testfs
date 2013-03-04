45585cbced95fd4b6aa5e47774a7aa7252cee5cf
raczzoli <racz.zoli@gmail.com>
2013-03-04 16:31:27 -0700

    added vfs generic functions to address space ops

--- aops.c
+++ aops.c
@@ -12,8 +12,9 @@ int testfs_get_block(struct inode *inode, sector_t iblock, struct buffer_head *b
 	int err = 0;
 	struct testfs_inode *testfs_inode = TESTFS_GET_INODE(inode); 
 
+	printk(KERN_INFO "testfs: testfs_get_block: inode number: %lu, mode: %d\n", inode->i_ino, inode->i_mode);
+
 	if (testfs_inode->block_ptr == 0) {
-		printk(KERN_INFO "testfs: testfs_get_block: block_ptr == 0, create=%d\n", create);
 		if (create == 0) 
 			return 0;
 
@@ -44,7 +45,6 @@ static int testfs_writepage(struct page *page, struct writeback_control *wbc)
 
 static int testfs_writepages(struct address_space *mapping, struct writeback_control *wbc)
 {
-	printk(KERN_INFO "testfs: testfs_writepages called\n");
 	return mpage_writepages(mapping, wbc, testfs_get_block);
 }
 
@@ -62,9 +62,34 @@ static int testfs_readpages(struct file *file, struct address_space *mapping,
 }
 
 
+static int testfs_write_begin(struct file *file, struct address_space *mapping,
+		loff_t pos, unsigned len, unsigned flags,
+		struct page **pagep, void **fsdata)
+{
+	return block_write_begin(mapping, pos, len, flags, pagep, testfs_get_block);
+}
+
+
+static int testfs_write_end(struct file *file, struct address_space *mapping,
+			loff_t pos, unsigned len, unsigned copied,
+			struct page *page, void *fsdata)
+{
+	return generic_write_end(file, mapping, pos, len, copied, page, fsdata);
+}
+
+
+static sector_t testfs_bmap(struct address_space *mapping, sector_t block)
+{
+	return generic_block_bmap(mapping,block,testfs_get_block);
+}
+
+
 const struct address_space_operations testfs_aops = {
         .writepage	= testfs_writepage,
 	.writepages	= testfs_writepages,
 	.readpage	= testfs_readpage,
-	.readpages	= testfs_readpages
+	.readpages	= testfs_readpages,
+	.write_begin	= testfs_write_begin,
+	.write_end	= testfs_write_end,
+	.bmap		= testfs_bmap
 };
