095ca27d22b253d223013b0862e6cc112d504b9b
raczzoli <racz.zoli@gmail.com>
2013-03-06 16:15:44 -0700

    added #include file.h

--- file.c
+++ file.c
@@ -1,6 +1,7 @@
 #include <linux/fs.h>
 #include <linux/quotaops.h>
 
+#include "file.h"
 
 int testfs_fsync(struct file *file, loff_t start, loff_t end, int datasync)
 {
@@ -8,14 +9,55 @@ int testfs_fsync(struct file *file, loff_t start, loff_t end, int datasync)
 }
 
 
+loff_t testfs_llseek(struct file *file, loff_t offset, int whence)
+{
+	int ret = 0;
+	ret = generic_file_llseek(file, offset, whence);
+
+	printk(KERN_INFO "testfs: testfs_llseek called...\n");
+
+	return ret;
+}
+
+
+ssize_t testfs_aio_read(struct kiocb *iocb, const struct iovec *iov, unsigned long nr_segs, loff_t pos)
+{
+	int ret = 0;
+	printk(KERN_INFO "testfs: testfs_aio_read\n");
+	ret = generic_file_aio_read(iocb, iov, nr_segs, pos);
+
+	return ret;
+}
+
+
+ssize_t testfs_sync_read(struct file *filp, char __user *buf, size_t len, loff_t *ppos)
+{
+	int ret = 0;
+	printk(KERN_INFO "testfs: testfs_sync_read\n");
+	ret = do_sync_read(filp, buf, len, ppos);
+
+	return ret;
+}
+
+
+ssize_t testfs_splice_read(struct file *in, loff_t *ppos, struct pipe_inode_info *pipe, size_t len, unsigned int flags)
+{
+	int ret = 0;
+	printk(KERN_INFO "testfs: testfs_splice_read\n");
+	ret = generic_file_splice_read(in, ppos, pipe, len, flags);
+
+	return ret;
+}
+
 /* file operations */
+
 const struct file_operations testfs_file_fops = {
-	.llseek 	= generic_file_llseek,
-	.aio_read 	= generic_file_aio_read,
+	.llseek 	= testfs_llseek,
+	.aio_read 	= testfs_aio_read,
 	.aio_write 	= generic_file_aio_write,
-	.read		= do_sync_read,
+	.read		= testfs_sync_read,
 	.write		= do_sync_write,
-	.splice_read	= generic_file_splice_read,
+	.splice_read	= testfs_splice_read,
 	.splice_write	= generic_file_splice_write,
 	.mmap		= generic_file_mmap,
 	.open		= dquot_file_open,
