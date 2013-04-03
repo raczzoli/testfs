4537eb53d04205f703794b551e8c912ee89c814e
raczzoli <racz.zoli@gmail.com>
2013-04-03 12:39:40 -0600

    added support for block groups

--- tools/create_fs.sh
+++ tools/create_fs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 umount /dev/loop0
-dd if=/dev/zero of=loopback.img bs=1024 count=204800
+dd if=/dev/zero of=loopback.img bs=100M count=2 
 losetup /dev/loop0 loopback.img
 
 /usr/bin/format /dev/loop0
--- tools/format.c
+++ tools/format.c
@@ -4,6 +4,14 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <string.h>
+#include <math.h>
+
+#define BLK_SIZE 	4096
+#define NUM_INODES	(BLK_SIZE * 8)
+#define ITABLE_SIZE	(NUM_INODES * sizeof(struct testfs_inode))
+#define ITABLE_NUM_BLKS ((int)ceil((double)ITABLE_SIZE / (double)BLK_SIZE))
+#define DATA_BLKS_SIZE	(BLK_SIZE * 8 * BLK_SIZE)
+#define BLK_GRP_SIZE 	((BLK_SIZE * 3) + ITABLE_SIZE + DATA_BLKS_SIZE)
 
 /* Commands :
  * $dd if=/dev/zero of=loopback.img bs=1024 count=204800
@@ -11,6 +19,7 @@
  * $format /dev/loop0
  */
 int fd;
+uint64_t disk_size = 0;
 
 struct testfs_superblock {
 	uint32_t magic;		/* Magic number */
@@ -26,6 +35,7 @@ struct testfs_superblock {
 struct testfs_inode {
 	uint16_t i_mode;
 	uint16_t i_size;
+	uint32_t group;
 	uint32_t block_ptr;
 };
 
@@ -38,6 +48,10 @@ struct testfs_dir_entry {
 
 int main(int argc, char *argv[])
 {
+
+	int num_groups, i = 0;
+	uint64_t write_pos = 0;
+
 	/* Read file system name from parameters */
 	if (argc != 2) {
 		printf("\nUsage : format [/dev/sda1]\n\n");
@@ -51,24 +65,38 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	if (write_super() < 0) {
-		goto err;
-	}
+	printf("Size of the disk in MB:");
+        scanf("%llu", &disk_size);
 
-	if (write_block_bitmap() < 0) {
-		goto err;
-	}
+        disk_size *= 1024 * 1024;
+	
+	num_groups = (int)(disk_size / (uint64_t)BLK_GRP_SIZE);
 
-	if (write_inode_bitmap() < 0) {
-		goto err;
-	}
+	for (i=0;i<num_groups;i++) {
+		write_pos = (uint64_t)i * (uint64_t)BLK_GRP_SIZE;
+		if (write_super(i) < 0) {
+			goto err;
+		}
 
-	if (write_itable() < 0) {
-		goto err;
-	}
+		if (write_group_descriptors(write_pos) < 0) {
+			goto err;
+		}
 
-	if (write_rootdir() < 0) {
-		goto err;
+		if (write_block_bitmap(write_pos) < 0) {
+			goto err;
+		}
+
+		if (write_inode_bitmap(write_pos) < 0) {
+			goto err;
+		}
+
+		if (write_itable(write_pos, i) < 0) {
+			goto err;
+		}
+
+		if (write_root_dir() < 0) {
+			goto err;
+		}
 	}
 
 	close(fd);
@@ -78,21 +106,21 @@ err:
 	exit(EXIT_FAILURE);
 }
 
-int write_super(void)
+int write_super(uint64_t write_pos)
 {
 	struct testfs_superblock sb;
 
-	sb.magic		= 0x1012F4DD;
-	sb.block_size	= 4096;
-	sb.block_count	= 32768;
-	sb.itable	= 3;
-	sb.itable_size	= 2;	
-	sb.block_bitmap = 1;
-	sb.inode_bitmap = 2;
+	sb.magic	= 0x1012F4DD;
+	sb.block_size	= BLK_SIZE;
+	sb.block_count	= 0; // not used
+	sb.itable	= 4;
+	sb.itable_size	= ITABLE_NUM_BLKS;	
+	sb.block_bitmap = 2;
+	sb.inode_bitmap = 3;
 	sb.rootdir_inode = 1;
 
 	/* Seek to block 0 */
-	if (lseek(fd, 0 * 4096, 0) < 0) {
+	if (lseek64(fd, write_pos + (uint64_t)(0 * BLK_SIZE), 0) < (uint64_t)0) {
 		perror("Failed to seek to superblock");
 		return -1;
 	}
@@ -104,9 +132,20 @@ int write_super(void)
 	return 0;
 }
 
-int write_block_bitmap(void)
+int write_group_descriptors(uint64_t write_pos)
 {
-	unsigned char bitmap[4096] = {0};
+	if (lseek64(fd, write_pos + (uint64_t)(1 * BLK_SIZE), 0) < (uint64_t)0) {
+                perror("Failed to seek to group descriptor table: ");
+                return -1;
+        }
+
+        write(fd, 0x00, BLK_SIZE);
+	return 0;
+}
+
+int write_block_bitmap(uint64_t write_pos)
+{
+	unsigned char bitmap[BLK_SIZE] = {0};
 	int c;
 
 	/* Resetting bitmap to dummy data */
@@ -118,13 +157,13 @@ int write_block_bitmap(void)
 	
 	/* zoli: If we want to mark the first X bytes as used, becuase of the big/little endian thing
 	 * we need to invert the order of 1s and 0s. So if we want to mark the first 5 blocks as used
-	 * we need to write it as 0xF8 = 11111000 not 0x1F = 00011111
+	 * we need to write it as 0xC0 = 11000000 not 0x3 = 00000011
 	 */
-	bitmap[0] = 0xF8;
+	bitmap[0] = 0xC0;
 
 	/* Seek to block 1 */
-	if (lseek(fd, 1 * 4096, 0) < 0) {
-		perror("Failed to seek to block bitmap");
+	if (lseek64(fd, write_pos + (uint64_t)(2 * BLK_SIZE), 0) < (uint64_t)0) {
+		perror("Failed to seek to block bitmap: ");
 		return -1;
 	}
 
@@ -133,9 +172,9 @@ int write_block_bitmap(void)
 	printf("Wrote block bitmap : %lu bytes\n", sizeof(bitmap));
 }
 
-int write_inode_bitmap(void)
+int write_inode_bitmap(uint64_t write_pos)
 {
-	unsigned char bitmap[4096] = {0};
+	unsigned char bitmap[BLK_SIZE] = {0};
 	int c;
 
 	/* Resetting bitmap to dummy data */
@@ -143,11 +182,13 @@ int write_inode_bitmap(void)
 		bitmap[c] = 0x00;
 	}
 
-	/* Marking 1st 4 inodes in use by ".","..", and the 3 test dirs */
-	bitmap[0] = 0xF0;
+	/* Marking 1st 3 inodes in use. The 2nd and 3rd inode is used by the ".",".." entries, 
+ 	 * and the the 1st is only marked as used because we don`t want to have inodes with i_ino 0 
+ 	 */
+	bitmap[0] = 0xE0;
 
 	/* Seek to block 2 */
-	if (lseek(fd, 2 * 4096, 0) < 0) {
+	if (lseek64(fd, write_pos + (uint64_t)(3 * BLK_SIZE), 0) < (uint64_t)0) {
 		perror("Failed to seek to inode bitmap");
 		return -1;
 	}
@@ -157,28 +198,28 @@ int write_inode_bitmap(void)
 	printf("Wrote inode bitmap : %lu bytes\n", sizeof(bitmap));
 }
 
-int write_itable(void)
+int write_itable(uint64_t write_pos, int group)
 {
-	struct testfs_inode itable[1024] = {0};
+	struct testfs_inode itable[NUM_INODES] = {0};
 	int c;
 
 	/* Resetting itable with dummy data */
-	for (c = 0; c < 1024; c++) {
+	for (c = 0; c < NUM_INODES; c++) {
 		if (c == 1) {
 			itable[c].i_mode 	= 0x41FF;
 			itable[c].i_size 	= 2 * sizeof(struct testfs_dir_entry);
-			itable[c].block_ptr 	= c + 4;
+			itable[c].group		= 0;
+			itable[c].block_ptr 	= ITABLE_NUM_BLKS + 4;
+			continue;
 		}
-		else {		
-			itable[c].i_mode 	= 0x41FF;	/* Mode = Dir */
-			itable[c].i_size 	= 0;		/* Size */
-			itable[c].block_ptr 	= 0;	/* Block Pointer */
-		}
-
+		itable[c].i_mode 	= 0x41FF;	/* Mode = Dir */
+		itable[c].i_size 	= 0;		/* Size */
+		itable[c].group		= group;	/* Block group */
+		itable[c].block_ptr 	= 0;		/* Block Pointer (relative to group) */
 	}
 
 	/* Seek to block 3 */
-	if (lseek(fd, 3 * 4096, 0) < 0) {
+	if (lseek64(fd, write_pos + (uint64_t)(4 * BLK_SIZE), 0) < (uint64_t)0) {
 		perror("Failed to seek to itable");
 		return -1;
 	}
@@ -188,7 +229,10 @@ int write_itable(void)
 	printf("Wrote itable : %lu bytes\n", sizeof(itable));
 }
 
-int write_rootdir(void)
+/*
+ * only called once, when writing data in the first group
+ * */
+int write_root_dir(void)
 {
 	struct testfs_dir_entry root[2];
 	int c;
@@ -206,15 +250,8 @@ int write_rootdir(void)
 	root[1].name[2] = '\0';
 	root[1].type = 1;		/* DT_DIR or DT_REG */
 
-//	for (c = 2; c < 5; c++) {
-//		root[c].inode_number = c;				/* Inode number */
-//		snprintf(root[c].name, sizeof(root[c].name), "testdir%d", c);
-//		root[c].name_len = (uint32_t)strlen(root[c].name);      /* Name length */
-//		root[c].type = 1;		/* DT_DIR or DT_REG */
-//	}
-
 	/* Seek to block 6 (1 + 6) */
-	if (lseek(fd, 5 * 4096, 0) < 0) {
+	if (lseek(fd, (4 + ITABLE_NUM_BLKS) * BLK_SIZE, 0) < 0) {
 		perror("Failed to seek to root directory data block");
 		return -1;
 	}
@@ -223,4 +260,3 @@ int write_rootdir(void)
 	write(fd, root, sizeof(root));
 	printf("Wrote root directory data : %lu bytes\n", sizeof(root));
 }
-
