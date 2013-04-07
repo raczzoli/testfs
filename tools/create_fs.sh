2210756e6bbd86fec5b64e2ee281ae79438fe13e
raczzoli <racz.zoli@gmail.com>
2013-04-07 09:36:31 -0600

    changed the size of the test partition to 200MB

--- tools/create_fs.sh
+++ tools/create_fs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 umount /dev/loop0
-dd if=/dev/zero of=loopback.img bs=100M count=2 
+dd if=/dev/zero of=loopback.img bs=100M count=3
 losetup /dev/loop0 loopback.img
 
 /usr/bin/format /dev/loop0
